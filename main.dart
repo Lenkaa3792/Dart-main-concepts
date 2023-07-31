743584f591fedcb0a63583320fe346cdabf06f6b
Lenkaa3792 <allanlenkaa@gmail.com>
2023-07-31 12:25:24 +0300

    working with strings

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
-void main(){
-  String name="Allan";
-  int age=25;
-  print("My name is "+name);
+void main() {
+  String name = "Allan";
+  int age = 25;
+  double unit = 4.1;
+  print("My age is " + name);
 }
---
+++ strings.dart
@@ -0,0 +1,7 @@
+void main() {
+  //lets work with string data type
+  String name = "john Doe";
+  String city = "nairobi";
+  //lets print this
+  print("My name is " + name + "." + "I live in " + city);
+}
