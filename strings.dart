05a90866f1b84a9e0a585cc3d21e2da03f764172
Lenkaa3792 <allanlenkaa@gmail.com>
2023-07-31 12:28:48 +0300

    String interpolation

--- strings.dart
+++ strings.dart
@@ -1,7 +1,9 @@
 void main() {
-  //lets work with string data type
+  //lets work with string data type and variables
   String name = "john Doe";
   String city = "nairobi";
+  String favfood = "Rice";
   //lets print this
   print("My name is " + name + "." + "I live in " + city);
+  print("My favorite food is ${favfood}");
 }
