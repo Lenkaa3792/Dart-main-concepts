a98182a8adcc128a07e6e4ce6a01776caaea5a7e
Lenkaa3792 <allanlenkaa@gmail.com>
2023-07-31 15:29:42 +0300

    working with strings

--- strings.dart
+++ strings.dart
@@ -6,4 +6,15 @@ void main() {
   //lets print this
   print("My name is " + name + "." + "I live in " + city);
   print("My favorite food is ${favfood}");
+  //length of strings(property of the string)
+  print(city.length);
+  //refer to specific character in the string using its index
+  print(city[5]);
+  //string functions
+  //to uppercase
+  print(city.toUpperCase());
+  //see if a character is inside the string
+  print(city.indexOf("b"));
+  print(name.indexOf("o"));
+  print(name.contains("e"));
 }
