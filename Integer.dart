5d9a3ff5b87f23c0c9a149a76f952f4e17c558c0
Lenkaa3792 <allanlenkaa@gmail.com>
2023-08-01 12:18:49 +0300

    math

--- Integer.dart
+++ Integer.dart
@@ -1,6 +1,15 @@
+import 'dart:async';
+
 void main() {
   int age = 25;
   double gpa = 3.9;
   print("my age is ${age}");
+
   print("my gpa is ${gpa}");
+  print(4 / 8);
+  print(gpa * age);
+  print(gpa / age);
+  print(gpa % age);
+  //modulo
+  print(10 % 2);
 }
