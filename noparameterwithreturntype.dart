66cafe87569b96890375adef4c180ea084f57f8f
Lenkaa3792 <allanlenkaa@gmail.com>
2023-09-13 14:14:42 +0300

    no parameter but there is return value

---
+++ noparameterwithreturntype.dart
@@ -0,0 +1,11 @@
+//no parameter but there is return value
+void main() {
+  // assigning function to a variable
+  String name = printFullName();
+  //print the value  of the variable
+  print(name);
+}
+// no parameter but there is return value 
+String printFullName() {
+  return "Aron Jacksoni";
+}
