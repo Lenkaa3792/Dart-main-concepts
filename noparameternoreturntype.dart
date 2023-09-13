e6af685f4e919eff5a87888f956ce9c438b791a4
Lenkaa3792 <allanlenkaa@gmail.com>
2023-09-13 13:49:26 +0300

    no return,no parameter

---
+++ noparameternoreturntype.dart
@@ -0,0 +1,9 @@
+// No parameter and no return type
+void main() {
+  printName();
+}
+
+//no parameters and no return type (nothing in parameter list and no return type)
+void printName() {
+  print("Aron Jackson: ");
+}
--- runes.dart
+++ runes.dart
@@ -1,7 +1,7 @@
 void main( )
 {
  var heart_symbol = '\u2665'; 
-var laugh_symbol = '\u{1f500}';
+var laugh_symbol = '\u{1f800}';
  print(heart_symbol);
  print(laugh_symbol);
  } 
