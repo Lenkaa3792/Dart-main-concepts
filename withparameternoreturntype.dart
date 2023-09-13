9422583e1fbd894ec9ce8cfcfe24182ae74fa0cc
Lenkaa3792 <allanlenkaa@gmail.com>
2023-09-13 13:59:16 +0300

    parameters and no return type

---
+++ withparameternoreturntype.dart
@@ -0,0 +1,9 @@
+//Here printFullName is a function  With a parameter and no return type function that takes the name of the parameter
+void main() {
+  printName("Aron Jackson");
+}
+
+// parameters and no return type
+void printName( String name ) {
+  print("My name is $name ");
+}
