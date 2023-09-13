379b36fa77fc59a121b13a331fd73d7a5495b4e1
Lenkaa3792 <allanlenkaa@gmail.com>
2023-09-13 15:44:15 +0300

    both parameter and return

---
+++ withparameterwithreturntype.dart
@@ -0,0 +1,11 @@
+import 'noparameterwithreturntype.dart';
+
+void main() {
+  // output the return value of printFullName
+  print(printFullName("Aron Jackson"));
+}
+
+// having parameters and return values
+String printFullName(String name) {
+  return "Aroni Jackson";
+}
